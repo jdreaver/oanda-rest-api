fa13174499c337929285a2be8406aa40ad38f50a
David Reaver <johndreaver@gmail.com>
2016-12-13 18:28:51 -0800

    Add sum type holding all transaction types

--- src/OANDA/Transactions.hs
+++ src/OANDA/Transactions.hs
@@ -15,6 +15,45 @@ newtype TradeID = TradeID { unTradeID :: Text }
 newtype OrderID = OrderID { unOrderID :: Text }
   deriving (Show, Eq, ToJSON, FromJSON)
 
+data TransactionType
+  = CREATE
+  | CLOSE
+  | REOPEN
+  | CLIENT_CONFIGURE
+  | CLIENT_CONFIGURE_REJECT
+  | TRANSFER_FUNDS
+  | TRANSFER_FUNDS_REJECT
+  | MARKET_ORDER
+  | MARKET_ORDER_REJECT
+  | LIMIT_ORDER
+  | LIMIT_ORDER_REJECT
+  | STOP_ORDER
+  | STOP_ORDER_REJECT
+  | MARKET_IF_TOUCHED_ORDER
+  | MARKET_IF_TOUCHED_ORDER_REJECT
+  | TAKE_PROFIT_ORDER
+  | TAKE_PROFIT_ORDER_REJECT
+  | STOP_LOSS_ORDER
+  | STOP_LOSS_ORDER_REJECT
+  | TRAILING_STOP_LOSS_ORDER
+  | TRAILING_STOP_LOSS_ORDER_REJECT
+  | ORDER_FILL
+  | ORDER_CANCEL
+  | ORDER_CANCEL_REJECT
+  | ORDER_CLIENT_EXTENSIONS_MODIFY
+  | ORDER_CLIENT_EXTENSIONS_MODIFY_REJECT
+  | TRADE_CLIENT_EXTENSIONS_MODIFY
+  | TRADE_CLIENT_EXTENSIONS_MODIFY_REJECT
+  | MARGIN_CALL_ENTER
+  | MARGIN_CALL_EXTEND
+  | MARGIN_CALL_EXIT
+  | DELAYED_TRADE_CLOSURE
+  | DAILY_FINANCING
+  | RESET_RESETTABLE_PL
+  deriving (Show, Eq)
+
+deriveJSON defaultOptions ''TransactionType
+
 data TimeInForce
   = GTC
   | GTD
@@ -140,7 +179,7 @@ data Transaction = Transaction
   , transactionAccountID :: AccountID
   , transactionUserID :: Integer
   , transactionBatchID :: TransactionID
-  , transactionType :: Text
+  , transactionType :: TransactionType
 
   -- Specific to individual transactions
   , transactionDivisionID :: Maybe Integer
